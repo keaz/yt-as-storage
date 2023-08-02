e3f4cd480c30621230268f04d5cb34fcd17ca06f
Kasun Ranasinghe <kranasingh@wiley.com>
2023-08-02 13:52:55 +0530

    Created input, output file handlers

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -6,36 +6,43 @@ use std::{
 
 use log::debug;
 
-pub struct FileHandler {
+pub struct InputHandler {
     file: File,
 }
 
-impl FileHandler {
-
-    pub fn new(file_path: &String) -> Self{
+impl InputHandler {
+    pub fn new(file_path: &String) -> Self {
         let path = Path::new(file_path);
-        // if path.exists() {
-        //     fs::remove_file(path).unwrap();
-        // }
-        // let file = File::create(path).unwrap();
         let file = File::open(path).unwrap();
         debug!("New file created {:?}", file);
-        FileHandler{
-            file
-        }
+        InputHandler { file }
     }
 
-
-    pub fn write_random(&mut self, offset: u64, buf: &[u8]) {
-        self.file.seek(SeekFrom::Start(offset)).unwrap();
-        self.file.write(buf).unwrap();
-    }
-
-    pub  fn read_random(&mut self, offset: u64, buf: &mut [u8]) -> bool {
+    pub fn read_random(&mut self, offset: u64, buf: &mut [u8]) -> bool {
         self.file.seek(SeekFrom::Start(offset)).unwrap();
         let read_data = self.file.read(buf).unwrap();
 
         read_data == 0
     }
+}
 
+pub struct OutputHandler {
+    file: File,
+}
+
+impl OutputHandler {
+    pub fn new(file_path: &String) -> Self {
+        let path = Path::new(file_path);
+        if path.exists() {
+            fs::remove_file(path).unwrap();
+        }
+        let file = File::create(path).unwrap();
+        debug!("New file created {:?}", file);
+        OutputHandler { file }
+    }
+
+    pub fn write_random(&mut self, offset: u64, buf: &[u8]) {
+        self.file.seek(SeekFrom::Start(offset)).unwrap();
+        self.file.write(buf).unwrap();
+    }
 }
